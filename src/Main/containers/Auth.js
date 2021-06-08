c899d4e441a264c9d202a0883682d52af89e0640
Jerome Cases <casesjeromem@gmail.com>
2021-06-08 16:27:38 +0800

    changes for Login component

--- src/Main/containers/Auth.js
+++ src/Main/containers/Auth.js
@@ -30,7 +30,7 @@ const Auth = (props) => {
           const { userId, username } = response.data.data;
           const userDetailsValue = { _id: userId, username: username };
           auth.userDetailsHandler(userDetailsValue);
-          auth.login();          
+          auth.login();
         })
         .catch((err) => console.log(err));
     } else {
@@ -68,62 +68,70 @@ const Auth = (props) => {
   function authSubmitHandler(e) {
     e.preventDefault();
     authenticateUser(user);
-    setUser({ username: "", password: "" });
-    resetValues();
+    setUser({ username: "", password: "" });    
   }
 
   return (
-    <div className="container-login">
-      <div className="login-form">
-        <main className="form-signin">
-          <form onSubmit={authSubmitHandler}>
-            <div className="mb-4 line-top" alt="" width="100%" />
-            <h1 className="h3 mb-5 fw-normal subtitle d-flex justify-content-center">
-              produktibo
-            </h1>
-            <Input
-              type="email"
-              id="username"
-              name="username"
-              divClass="form-floating mb-1 mx-6"
-              placeholder="name@example.com"
-              elementType="input"
-              isLabelVisible={true}
-              label="Username"
-              onInput={authInputHandler}
-            />
-            <Input
-              type="password"
-              id="password"
-              name="password"
-              divClass="form-floating mb-3 mx-6"
-              placeholder=""
-              elementType="input"
-              isLabelVisible={true}
-              label="Password"
-              onInput={authInputHandler}
-            />
-            <div className="px-6 mb-1">
-              <button className="w-100 btn btn-lg btn-success" type="submit">
-                {isRegistered ? "Sign in" : "Continue"}
-              </button>
-            </div>
-            <p className="mt-5 mb-1 text-muted d-flex justify-content-center fs-6">
-              {isRegistered
-                ? "Don't have an account?"
-                : "Already have an account?"}
-            </p>
-          </form>
-          <div className="mb-3 d-flex justify-content-center ">
-            <button
-              className="fs-5 signup-text"
-              onClick={switchModeHandler}
-              style={{ border: 0 }}
-            >
-              {isRegistered ? "Create account" : "Sign in"}
-            </button>
+    <div className="container-fluid">
+      <div className="row mt-6">
+        <div className="col-lg-4 col-md-2"></div>
+        <div className="col-lg-4 col-md-8">
+          <div className="login-form">
+            <main className="form-signin">
+              <form onSubmit={authSubmitHandler}>
+                <div className="mb-4 line-top" alt="" width="100%" />
+                <h1 className="h3  fw-normal title d-flex justify-content-center">
+                  produktibo
+                </h1>
+                <p className="text-muted mb-5 subtitle">Improve your productivity</p>
+                <Input
+                  type="email"
+                  id="username"
+                  name="username"
+                  divClass="form-floating mb-1 mx-6"
+                  placeholder="name@example.com"
+                  elementType="input"
+                  isLabelVisible={true}
+                  label="Username"
+                  onInput={authInputHandler}
+                />
+                <Input
+                  type="password"
+                  id="password"
+                  name="password"
+                  divClass="form-floating mb-3 mx-6"
+                  placeholder=""
+                  elementType="input"
+                  isLabelVisible={true}
+                  label="Password"
+                  onInput={authInputHandler}
+                />
+                <div className="px-6 mb-1">
+                  <button
+                    className="w-100 btn btn-lg btn-success"
+                    type="submit"
+                  >
+                    {isRegistered ? "Sign in" : "Continue"}
+                  </button>
+                </div>
+                <p className="mt-5 mb-1 text-muted d-flex justify-content-center fs-6">
+                  {isRegistered
+                    ? "Don't have an account?"
+                    : "Already have an account?"}
+                </p>
+              </form>
+              <div className="mb-3 d-flex justify-content-center ">
+                <button
+                  className="fs-5 signup-text auth-option-button"
+                  onClick={switchModeHandler}                  
+                >
+                  {isRegistered ? "Create account" : "Sign in"}
+                </button>
+              </div>
+            </main>
           </div>
-        </main>
+        </div>
+        <div className="col-lg-4 col-md-2"></div>
       </div>
     </div>
   );
